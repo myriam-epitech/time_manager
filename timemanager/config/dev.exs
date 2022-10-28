e7a374d34e9e5cf167e00101073dbb1cd6dfe6ae
myriam-epitech <myriam1.abdelli@epitech.eu>
2022-10-29 00:10:02 +0200

    [FIX] clean code

--- timemanager/config/dev.exs
+++ timemanager/config/dev.exs
@@ -2,10 +2,10 @@ import Config
 
 # Configure your database
 config :timemanager, Timemanager.Repo,
-  username: System.get_env("POSTGRES_USERNAME"),
-  password: System.get_env("POSTGRES_PASSWORD"),
-  hostname: System.get_env("POSTGRES_HOSTNAME"),
-  database: System.get_env("POSTGRES_DATABASE"),
+  username: System.get_env("POSTGRES_USERNAME") || "postgres",
+  password: System.get_env("POSTGRES_PASSWORD") || "postgres",
+  hostname: System.get_env("POSTGRES_HOSTNAME") || "localhost",
+  database: System.get_env("POSTGRES_DATABASE") || "timemanager_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
--- timemanager/mix.exs
+++ timemanager/mix.exs
@@ -44,7 +44,7 @@ defmodule Timemanager.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:cors_plug, "~> 3.0"},
+      {:cors_plug, "~> 2.0"}
     ]
   end
 
