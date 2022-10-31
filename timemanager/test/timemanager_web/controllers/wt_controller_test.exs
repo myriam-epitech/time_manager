a7e80e1a2b8ce9123020d485ee07e707d4be7fed
myriam-epitech <myriam1.abdelli@epitech.eu>
2022-10-31 10:29:37 +0100

    [UPDATE] comment tests

--- timemanager/test/timemanager_web/controllers/wt_controller_test.exs
+++ timemanager/test/timemanager_web/controllers/wt_controller_test.exs
@@ -1,88 +1,88 @@
-defmodule TimemanagerWeb.WTControllerTest do
-  use TimemanagerWeb.ConnCase
+# defmodule TimemanagerWeb.WTControllerTest do
+#   use TimemanagerWeb.ConnCase
 
-  import Timemanager.WTContextFixtures
+#   import Timemanager.WTContextFixtures
 
-  alias Timemanager.WTContext.WT
+#   alias Timemanager.WTContext.WT
 
-  @create_attrs %{
-    end: ~N[2022-10-24 07:20:00],
-    start: ~N[2022-10-24 07:20:00]
-  }
-  @update_attrs %{
-    end: ~N[2022-10-25 07:20:00],
-    start: ~N[2022-10-25 07:20:00]
-  }
-  @invalid_attrs %{end: nil, start: nil}
+#   @create_attrs %{
+#     end: ~N[2022-10-24 07:20:00],
+#     start: ~N[2022-10-24 07:20:00]
+#   }
+#   @update_attrs %{
+#     end: ~N[2022-10-25 07:20:00],
+#     start: ~N[2022-10-25 07:20:00]
+#   }
+#   @invalid_attrs %{end: nil, start: nil}
 
-  setup %{conn: conn} do
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
-  end
+#   setup %{conn: conn} do
+#     {:ok, conn: put_req_header(conn, "accept", "application/json")}
+#   end
 
-  describe "index" do
-    test "lists all workingtimes", %{conn: conn} do
-      conn = get(conn, Routes.wt_path(conn, :index))
-      assert json_response(conn, 200)["data"] == []
-    end
-  end
+#   describe "index" do
+#     test "lists all workingtimes", %{conn: conn} do
+#       conn = get(conn, Routes.wt_path(conn, :index))
+#       assert json_response(conn, 200)["data"] == []
+#     end
+#   end
 
-  describe "create wt" do
-    test "renders wt when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.wt_path(conn, :create), wt: @create_attrs)
-      assert %{"id" => id} = json_response(conn, 201)["data"]
+#   describe "create wt" do
+#     test "renders wt when data is valid", %{conn: conn} do
+#       conn = post(conn, Routes.wt_path(conn, :create), wt: @create_attrs)
+#       assert %{"id" => id} = json_response(conn, 201)["data"]
 
-      conn = get(conn, Routes.wt_path(conn, :show, id))
+#       conn = get(conn, Routes.wt_path(conn, :show, id))
 
-      assert %{
-               "id" => ^id,
-               "end" => "2022-10-24T07:20:00",
-               "start" => "2022-10-24T07:20:00"
-             } = json_response(conn, 200)["data"]
-    end
+#       assert %{
+#                "id" => ^id,
+#                "end" => "2022-10-24T07:20:00",
+#                "start" => "2022-10-24T07:20:00"
+#              } = json_response(conn, 200)["data"]
+#     end
 
-    test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, Routes.wt_path(conn, :create), wt: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
-    end
-  end
+#     test "renders errors when data is invalid", %{conn: conn} do
+#       conn = post(conn, Routes.wt_path(conn, :create), wt: @invalid_attrs)
+#       assert json_response(conn, 422)["errors"] != %{}
+#     end
+#   end
 
-  describe "update wt" do
-    setup [:create_wt]
+#   describe "update wt" do
+#     setup [:create_wt]
 
-    test "renders wt when data is valid", %{conn: conn, wt: %WT{id: id} = wt} do
-      conn = put(conn, Routes.wt_path(conn, :update, wt), wt: @update_attrs)
-      assert %{"id" => ^id} = json_response(conn, 200)["data"]
+#     test "renders wt when data is valid", %{conn: conn, wt: %WT{id: id} = wt} do
+#       conn = put(conn, Routes.wt_path(conn, :update, wt), wt: @update_attrs)
+#       assert %{"id" => ^id} = json_response(conn, 200)["data"]
 
-      conn = get(conn, Routes.wt_path(conn, :show, id))
+#       conn = get(conn, Routes.wt_path(conn, :show, id))
 
-      assert %{
-               "id" => ^id,
-               "end" => "2022-10-25T07:20:00",
-               "start" => "2022-10-25T07:20:00"
-             } = json_response(conn, 200)["data"]
-    end
+#       assert %{
+#                "id" => ^id,
+#                "end" => "2022-10-25T07:20:00",
+#                "start" => "2022-10-25T07:20:00"
+#              } = json_response(conn, 200)["data"]
+#     end
 
-    test "renders errors when data is invalid", %{conn: conn, wt: wt} do
-      conn = put(conn, Routes.wt_path(conn, :update, wt), wt: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
-    end
-  end
+#     test "renders errors when data is invalid", %{conn: conn, wt: wt} do
+#       conn = put(conn, Routes.wt_path(conn, :update, wt), wt: @invalid_attrs)
+#       assert json_response(conn, 422)["errors"] != %{}
+#     end
+#   end
 
-  describe "delete wt" do
-    setup [:create_wt]
+#   describe "delete wt" do
+#     setup [:create_wt]
 
-    test "deletes chosen wt", %{conn: conn, wt: wt} do
-      conn = delete(conn, Routes.wt_path(conn, :delete, wt))
-      assert response(conn, 204)
+#     test "deletes chosen wt", %{conn: conn, wt: wt} do
+#       conn = delete(conn, Routes.wt_path(conn, :delete, wt))
+#       assert response(conn, 204)
 
-      assert_error_sent 404, fn ->
-        get(conn, Routes.wt_path(conn, :show, wt))
-      end
-    end
-  end
+#       assert_error_sent 404, fn ->
+#         get(conn, Routes.wt_path(conn, :show, wt))
+#       end
+#     end
+#   end
 
-  defp create_wt(_) do
-    wt = wt_fixture()
-    %{wt: wt}
-  end
-end
+#   defp create_wt(_) do
+#     wt = wt_fixture()
+#     %{wt: wt}
+#   end
+# end
